b2a0a197340b68352ccb0db39b28768f9f9b6bc4
Matthew Eng <matthew.eng2@gmail.com>
2021-02-26 14:07:35 -0500

    Add yelp datasets: user, tip, covid features, checkin to staging and ods

--- L2/yelp_covid/ods_yelp.sql
+++ L2/yelp_covid/ods_yelp.sql
@@ -6,6 +6,7 @@ create or replace file format json_format
 
 -- Load data from staging to ODS
 
+-- Yelp Business
 create or replace table yelp_business (
   business_id string not null unique,
   name string,
@@ -52,7 +53,7 @@ SELECT
     SRC:hours
 FROM staging.yelp_business_raw;
 
-
+--  Yelp Reviews
 create or replace table yelp_reviews (
   review_id string not null unique,
   user_id string,
@@ -79,3 +80,119 @@ SELECT
     SRC:text,
     SRC:date
 FROM staging.yelp_reviews_raw;
+
+--  Yelp Checkin
+create or replace table yelp_checkin (
+  business_id string not null unique,
+  date string,
+  constraint pk_business_id primary key (business_id),
+  constraint fk_business_id foreign key (business_id) references yelp_business(business_id)
+);
+
+INSERT INTO yelp_checkin
+SELECT
+    SRC:business_id,
+    SRC:date
+FROM staging.yelp_checkin_raw;
+
+--  Yelp User
+create or replace table yelp_user (
+  user_id string not null unique,
+  name string,
+  review_count numeric,
+  yelping_since timestamp,
+  useful numeric,
+  funny numeric,
+  cool numeric,
+  elite string,
+  friends string,
+  fans numeric,
+  average_stars numeric,
+  compliment_hot numeric,
+  compliment_more numeric,
+  compliment_profile numeric,
+  compliment_cute numeric,
+  compliment_list numeric,
+  compliment_note numeric,
+  compliment_plain numeric,
+  compliment_cool numeric,
+  compliment_funny numeric,
+  compliment_writer numeric,
+  compliment_photos numeric,
+  constraint pk_user_id primary key (user_id)
+);
+
+INSERT INTO yelp_user
+SELECT
+  SRC:user_id,
+  SRC:name,
+  SRC:review_count,
+  SRC:yelping_since,
+  SRC:useful,
+  SRC:funny,
+  SRC:cool,
+  SRC:elite,
+  SRC:friends,
+  SRC:fans,
+  SRC:average_stars,
+  SRC:compliment_hot,
+  SRC:compliment_more,
+  SRC:compliment_profile,
+  SRC:compliment_cute,
+  SRC:compliment_list,
+  SRC:compliment_note,
+  SRC:compliment_plain,
+  SRC:compliment_cool,
+  SRC:compliment_funny,
+  SRC:compliment_writer,
+  SRC:compliment_photos
+FROM staging.yelp_user_raw;
+
+-- Yelp Tip
+create or replace table yelp_tip (
+  user_id string not null unique,
+  business_id string,
+  text string,
+  date timestamp,
+  compliment_count numeric,
+  constraint pk_user_id primary key (user_id),
+  constraint fk_user_id foreign key (user_id) references yelp_user(user_id),
+  constraint fk_business_id foreign key (business_id) references yelp_business(business_id)
+);
+
+INSERT INTO yelp_tip
+SELECT
+  SRC:user_id,
+  SRC:business_id,
+  SRC:text,
+  SRC:date,
+  SRC:compliment_count
+FROM staging.yelp_tip_raw;
+
+-- Yelp Covid Features
+create or replace table yelp_covid_features (
+  business_id string not null unique,
+  highlights string,
+  delivery_or_takeout string,
+  grubhub_enabled string,
+  call_to_action_enabled string,
+  request_quote_enabled string,
+  covid_banner string,
+  temporary_closed_until string,
+  virtual_services string,
+  constraint pk_business_id primary key (business_id),
+  constraint fk_business_id foreign key (business_id) references yelp_business(business_id)
+);
+
+INSERT INTO yelp_covid_features
+SELECT
+    SRC:business_id,
+    SRC:highlights,
+    SRC:delivery_or_takeout,
+    SRC:grubhub_enabled,
+    SRC:call_to_action_enabled,
+    SRC:request_quote_enabled,
+    SRC:covid_banner,
+    SRC:temporary_closed_until,
+    SRC:virtual_services
+FROM staging.yelp_covid_features_raw;
--- L2/yelp_covid/staging_yelp.sql
+++ L2/yelp_covid/staging_yelp.sql
@@ -11,7 +11,23 @@ create or replace table yelp_business_raw (
   src variant
 );
 
-create or replace table yelp_reviews_raw (
+create or replace table yelp_review_raw (
+  src variant
+);
+
+create or replace table yelp_checkin_raw (
+  src variant
+);
+
+create or replace table yelp_user_raw (
+  src variant
+);
+
+create or replace table yelp_tip_raw (
+  src variant
+);
+
+create or replace table yelp_covid_features_raw (
   src variant
 );
 
@@ -20,4 +36,16 @@ put file:///Users/mattheweng/Downloads/yelp_dataset/yelp_academic_dataset_busine
 copy into yelp_business_raw from @staging/yelp_academic_dataset_business.json  file_format = (format_name =json_format) on_error = 'skip_file';
 
 put file:///Users/mattheweng/Downloads/yelp_dataset/yelp_academic_dataset_review.json @staging auto_compress=true;
-copy into yelp_reviews_raw from @staging/yelp_academic_dataset_review.json  file_format = (format_name =json_format) on_error = 'skip_file';
+copy into yelp_review_raw from @staging/yelp_academic_dataset_review.json  file_format = (format_name =json_format) on_error = 'skip_file';
+
+put file:///Users/mattheweng/Downloads/yelp_dataset/yelp_academic_dataset_checkin.json @staging auto_compress=true;
+copy into yelp_checkin_raw from @staging/yelp_academic_dataset_checkin.json  file_format = (format_name =json_format) on_error = 'skip_file';
+
+put file:///Users/mattheweng/Downloads/yelp_dataset/yelp_academic_dataset_user.json @staging auto_compress=true;
+copy into yelp_user_raw from @staging/yelp_academic_dataset_user.json  file_format = (format_name =json_format) on_error = 'skip_file';
+
+put file:///Users/mattheweng/Downloads/yelp_dataset/yelp_academic_dataset_tip.json @staging auto_compress=true;
+copy into yelp_tip_raw from @staging/yelp_academic_dataset_tip.json  file_format = (format_name =json_format) on_error = 'skip_file';
+
+put file:///Users/mattheweng/Downloads/covid_19_dataset_2020_06_10/yelp_academic_dataset_covid_features.json @staging auto_compress=true;
+copy into yelp_covid_features_raw from @staging/yelp_academic_dataset_covid_features.json  file_format = (format_name =json_format) on_error = 'skip_file';
